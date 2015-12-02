bf9e2ee3f80b8e30f25b3a568fe5463bced41894
Jesus Domenech <jesusjda@gmail.com>
2015-12-02 14:32:49 +0100

    p3 finalizada

--- concurrente/p3/spawner.erl
+++ concurrente/p3/spawner.erl
@@ -1,5 +1,6 @@
 -module(spawner).
 -export([my_spawn/3,my_spawn_monitor/3,my_spawn_on_exit/3,my_spawn_time/4,proceso/0]).
+-export([my_spawn_alive/3,my_spawn_alive_N/4,my_spawn_one_death_N/4]).
 
 % -----------
 % ejercicio 1
@@ -104,30 +105,140 @@ my_spawn_time(Mod,Func,Args,Time) ->
 	Pid.
 
 % -----------
-% ejercicio 3
+% ejercicio 4
 % -----------
-my_spawn_alive(Mod,Func,Args,Time) ->
+% utilizar la funcion "proceso" para este apartado (igual que en los anteriores)
+my_spawn_alive(Mod,Func,Args) ->
+    % se crea el proceso
     Pid = spawn(Mod,Func,Args),
-    monitoriza(Pid,Mod,Func,Args),
+    % se monitoriza
+    spawn(fun()->monitoriza(Pid,Mod,Func,Args) end),
     Pid.
 
 monitoriza(Pid,Mod,Func,Args) ->
-    MonRef = erlang:monitor(process, Pid),
+    % al recibir un Pid lo monitoriza
+    erlang:monitor(process, Pid),
     receive
-	{'DOWN',MonRef,process,Pid,Reason} -> 
-	    io:format("R: ~p -> ~p~n",[Pid,Reason]),
+	% este caso es solo para poder ejecutar las pruebas
+	stop ->
+	    Pid!stop,
+	    io:format("END~n",[]);
+	% si recibe una muerte del proceso lo relanza
+	X ->
+	    io:format("R: ~p~n",[X]),
 	    NewPid = spawn(Mod,Func,Args),
-	    io:format("Echo pid ~p~n",[NewPid]),
-	    monitoriza(Pid,Mod,Func,Args);
-	{'DOWN',MonRef,process,Pid,normal} -> 
-	    io:format("END~n",[])
+	    io:format("Relanza con pid ~p~n",[NewPid]),
+	    % realiza el bucle con el nuevo Pid del proceso
+	    monitoriza(NewPid,Mod,Func,Args)
     end.
-	     
 
+% -----------
+% ejercicio 5
+% -----------
+% para simplificar la llamada se lanzará N veces la misma funcion
+% para generalizarlo debe recibir una lista de modulos funciones y argumentos
+my_spawn_alive_N(N,Mod,Func,Args) ->
+    Monitor = spawn(fun() ->
+			   monitor_N([],[],[],[])
+		    end),
+    spawn_N(Monitor,N,Mod,Func,Args).
 
+% si fueran N diferentes la cabecera sería:
+% spawn_N(Monitor,1,[Mod],[Func],[Args]) ->
+spawn_N(Monitor,1,Mod,Func,Args) ->
+    Pid = spawn(Mod,Func,Args),
+    Monitor!{monitoriza,Pid,Mod,Func,Args},
+    ok;
+% si fueran N diferentes la cabecera sería:
+% spawn_N(Monitor,N,[Mod|Mods],[Func|Funcs],[Args|Argss]) ->
+spawn_N(Monitor,N,Mod,Func,Args) ->
+    Pid = spawn(Mod,Func,Args),
+    Monitor!{monitoriza,Pid,Mod,Func,Args},
+    % para N diferentes la llamada recursiva se realiza con el resto de la lista
+    spawn_N(Monitor,N-1,Mod,Func,Args).
+    
+monitor_N(Pids,Mods,Funcs,Argss)->
+    receive
+	{monitoriza,Pid,M,F,A} ->
+	    io:format("monitorizando ~p~n",[Pid]),
+	    erlang:monitor(process, Pid),
+	    % y lo añadimos a la lista para poder relanzarlo
+	    monitor_N([Pid|Pids],[M|Mods],[F|Funcs],[A|Argss]);
+	{'DOWN',_,process,Pid,Reason} ->
+	    io:format("Pid ~p ha muerto. Razon: ~p~n",[Pid,Reason]),
+	    % relaunch modifica las listas borrando el antiguo pid
+	    {NewPid,Ps,Ms,Fs,As} = relaunch(Pid,Pids,Mods,Funcs,Argss),
+	    erlang:monitor(process,NewPid),
+	    io:format("Relanzado con nuevo Pid: ~p~n",[NewPid]),
+	    monitor_N(Ps,Ms,Fs,As);
+	stop ->
+	    io:format("adios~n",[])
+    end.
+
+% relaunch busca el pid en la lista de pids
+% y va avanzando en el resto para poder relanzar
+% salva una copia de la cabecera para poder devolver 
+% la lista completa actualizada
+relaunch(P,Ps,Ms,Fs,As) ->
+    relaunch(P,[],Ps,[],Ms,[],Fs,[],As).
+
+% Xi es la lista izquierda de X
+% Xa es el elemento actual de X
+% Xd es la lista derecha de X
+% P es el Pid buscado
+% Nunca deberia pasar del caso en que Xd = []
+relaunch(P,Pi,[Pa|Pd],Mi,[Ma|Md],Fi,[Fa|Fd],Ai,[Aa|Ad]) ->
+    if 
+	P == Pa ->
+	    NewPid = spawn(Ma,Fa,Aa),
+	    {NewPid,Pi++[NewPid|Pd],Mi++[Ma|Md],Fi++[Fa|Fd],Ai++[Aa|Ad]};
+	true ->
+	    relaunch(P,Pi++[Pa],Pd,Mi++[Ma],Md,Fi++[Fa],Fd,Ai++[Aa],Ad)
+    end.
+
+% -----------
+% ejercicio 6
+% -----------
+% para simplificar la llamada se lanzará N veces la misma funcion
+% para generalizarlo debe recibir una lista de modulos funciones y argumentos
+% igual que en el ejercicio 5
+my_spawn_one_death_N(N,Mod,Func,Args) ->
+    Monitor = spawn(fun() ->
+			   monitor_one_death_N([])
+		    end),
+    spawn_N(Monitor,N,Mod,Func,Args).
 	
+
+monitor_one_death_N(Pids) ->
+    receive
+	{monitoriza,Pid,_,_,_} ->
+	    io:format("monitorizando ~p~n",[Pid]),
+	    erlang:monitor(process, Pid),
+	    % y lo añadimos a la lista para poder matarlos
+	    monitor_one_death_N([Pid|Pids]);
+	{'DOWN',_,process,Pid,Reason} ->
+	    io:format("Pid ~p ha muerto. Razon: ~p~n",[Pid,Reason]),
+	    % manda un kill a toda la lista menos a Pid
+	    kill_all_except_one(Pid,Pids);
+	stop ->
+	    io:format("adios~n",[])
+    end.
+kill_all_except_one(Pid,[]) ->
+    io:format("Todos matados excepto ~p que ya estaba muerto~n",[Pid]);
+
+kill_all_except_one(Pid,[P|Pids]) ->
+    if
+	Pid == P ->
+	    kill_all_except_one(Pid,Pids);
+	true ->
+	    exit(P, kill),
+	    kill_all_except_one(Pid,Pids)
+    end.
+    
+
+
 proceso() ->
-	io:format("Estoy vivo"),
+	io:format("Estoy vivo ~p~n",[self()]),
 	receive
 		stop -> 
 			exit("por stop")
