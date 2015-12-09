102ef136d29d3acd7a708c3efd9dd5842b1c64e0
Jesus Domenech <jesusjda@gmail.com>
2015-12-09 15:25:54 +0100

    p4 done?

--- concurrente/p4/player.erl
+++ concurrente/p4/player.erl
@@ -1,65 +1,95 @@
 -module(player).
 % export basics
--export([start/1,start/2]).
--export([loop/1]).
+-export([start/1,start/2,start/3]).
+-export([loop/2]).
 
 
 start(Node) ->
-    start(Node,federico).
+    start(Node,serv,federico).
+start(Node,{name,Name}) ->
+    start(Node,serv,Name);
+start(Node,{server,Serv}) ->
+    start(Node,Serv,federico).
 
-start(Node,Name)->
+start(Node,Server,Name)->
     Self = self(),
-    {serverGame,Node} ! {Self,{join,Name}},
+    {Server,Node} ! {Self,{join,Name}},
     receive
 	{From,ok} ->
 	    io:format("Connected!~n",[]),
-	    loop(From,Name)
+	    loop(From,Name);
+	{_,error,Reason} ->
+	    io:format("Impossible! Reason: ~p~n",[Reason])
+    after 2000 ->
+	    io:format("Impossible! Reason: Unrecharchable~n",[])
     end.
 
+
+
 loop(From,Name) ->
+    Self = self(),
     receive
+	stop ->
+	    io:format("Bye Bye~n");
+	points ->
+	    From ! {Self,{getpoints,Name}},
+	    loop(From,Name);
+	stats ->
+	    From ! {Self,{getpoints,all}},
+	    loop(From,Name);
+	{stats,Stats} ->
+	    io:format("~nPoints: ~p~n",[Stats]),
+	    loop(From,Name);
 	{endGame,Text,Stats} ->
 	    io:format("~p",[Text]),
 	    print_stats(Stats);
 	{From,{word,W}} ->
-	    io:format("New word: ~p~n",[W]),
+	    write_word(From,Self,W),
 	    loop(From,Name);
 	{From,{start,W}} ->
 	    io:format("Game Start!!!~n",[]),
-	    io:format("New word: ~p~n",[W]),
-	    write_word(From),
+	    write_word(From,Self,W),
 	    loop(From,Name);
-	{From,ping} ->
-	    From ! pong,
+	{From,ping,Respond} ->
+	    Respond ! {Name,pong},
 	    loop(From,Name);
-	{From,{answer,error,W}} ->
+	{_,{answer,error,W}} ->
 	    io:format("Sorry!!~n",[]),
-	    io:format("Current word: ~p~n",[W]),
-	    write_word(From),
+	    write_word(From,Self,W),
 	    loop(From,Name);
-	{From,{answer,correct}} ->
-	    io:format("Congratulations!!~n",[]),
+	{From,{answer,correct,Points}} ->
+	    io:format("Congratulations yo win ~p points!~n",[Points]),
 	    loop(From,Name);
 	{From,{Player,wins}} ->
 	    if Player =/= Name ->
-		    io:format("Sorry, player ~p wins~n",[Player]);
+		    io:format("~nSorry, player ~p wins~n",[Player]);
 	       true ->
 		    ok
 	    end,
 	    loop(From,Name);
-	dummy ->
+	X ->
+	    io:format("F:~p~n~p~n",[From,X]),
 	    loop(From,Name)
-		%%RECIBIR respuesta del envio de la palabra y hacer write word
     end.
 
 
-write_word(From) ->
-     spawn(fun() ->
-		   io:format("Write ~p",[W]),
-		   W0 = io:get_line("> "),
-		   W1 = string:substr(W0,1,string:len(W0)-1),
-		   From ! {self(),{word,W1}}
-	   end).
+write_word(From,Self,W) ->
+    spawn(fun() ->
+		  io:format("Write ~p",[W]),
+
+		  W0 = io:get_line("> "),
+
+		  Token = string:substr(W0,1,1),
+		  if Token =/= "#" ->
+			  W1 = string:substr(W0,1,string:len(W0)-1),			  
+			  From ! {Self,{word,W1}};
+		     true ->
+			  W1 = string:substr(W0,2,string:len(W0)-2),			  
+			  io:format("~p~n",[W1]),
+			  Self ! list_to_atom(W1),
+			  write_word(From,Self,W)
+		  end
+	  end).
        
 print_stats([]) ->
     io:format("-------------~n",[]);
--- concurrente/p4/server.erl
+++ concurrente/p4/server.erl
@@ -1,8 +1,8 @@
 -module(server).
-% export interface -> serverGame
--export([start/0,players/0,addWord/1,points/0,stop/0]).
+% export interface -> serv
+-export([start/0,players/0,addWord/1,points/0,player_points/1,stop/0]).
 % export generic interface 
--export([start/1,players/1,addWord/2,points/1,stop/1]).
+-export([start/1,players/1,addWord/2,points/1,player_points/2,stop/1]).
 % export others
 -export([loop/0]).
 
@@ -10,22 +10,26 @@
 %%                       Words Game                       %%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-%%% Interface -> serverGame
+%%% Interface -> serv
 
 start() ->
-    start(serverGame).
+    spawner:start(),
+    start(serv).
 
 players() ->
-    players(serverGame).
+    players(serv).
 
 addWord(Word) ->
-    addWord(serverGame,Word).
+    addWord(serv,Word).
 
 points() ->
-    points(serverGame).
+    points(serv).
+
+player_points(Player) ->
+    player_points(serv,Player).
 
 stop() ->
-    stop(serverGame).
+    stop(serv).
 
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -43,7 +47,10 @@ addWord(Server, Word)->
     Server ! {addWord,Word}.
 
 points(Server) ->
-    Server ! {self(),{getPoints,all}},
+    player_points(Server,all).
+
+player_points(Server,Player) ->
+    Server ! {self(),{getPoints,Player}},
     receive
 	{_,points,Points} ->
 	    io:format("~p~n",[Points])
@@ -58,11 +65,7 @@ stop(Server) ->
 %%% Server
 
 loop(Players, Words, Actual) ->
-    if Actual =/= undefined ->
-	    io:format("Word ~p~n",[Actual]);
-       true ->
-	    io:format("Ready!~n",[])
-    end,
+    Self = self(),
     receive
 	play ->
 	    if Actual =/= undefined ->
@@ -72,44 +75,65 @@ loop(Players, Words, Actual) ->
 		    loop(Players,Words,W)
 	    end;
 	players ->
-	    Players,
+	    io:format("players: ~p~n",[Players]),
 	    loop(Players,Words,Actual);
 	stop ->
 	    Stats = get_stadistics(Players,all),
-	    send_all(Players,{endGame,"bye, thanks for playing.~n",Stats}),
+	    async_send_all(Players,{endGame,"bye, thanks for playing.~n",Stats}),
 	    io:format("bye, thanks for playing.~n",[]);
+	{addWord,W} ->
+	    loop(Players,[W|Words],Actual);
+	{removeWord,W} ->
+	    loop(Players,[W|Words],Actual);
+	{removePlayer,Name} ->
+	    Play = remove_player(Players,Name),
+	    loop(Play,Words,Actual);
 	{From,{getpoints,Player}} ->
 	    Stats = get_stadistics(Players,Player),
-	    From ! {stats,Stats};
+	    From ! {stats,Stats},
+	    loop(Players,Words,Actual);
 	{From,{join,Name}} ->
-	    io:format("New player: ~p (~p)~n",[Name,From]),
-	    From ! {self(),ok},
-	    loop([{From,Name,0}|Players],Words,Actual);
+	    Exist = check_player_exists(Players,Name),
+	    if Exist =/= true ->
+		    io:format("New player: ~p (~p)~n",[Name,From]),
+		    From ! {Self,ok},
+		    if Actual =/= undefined ->
+			    From ! {Self,{word,Actual}};
+		       true ->
+			    ok
+		    end,
+		    loop([{From,Name,0}|Players],Words,Actual);
+	       true ->
+		    io:format("Player name repeated: ~p (~p)~n",[Name,From]),
+		    From ! {Self,error,"This name already exists!"},
+		    loop(Players,Words,Actual)
+	    end;
 	{From,{word,W}} ->
 	    if W =/= Actual ->
-		    From ! {self(),{answer,error,Actual}};
+		    From ! {Self,{answer,error,Actual}},
+		    loop(Players,Words,Actual);
 	       true ->
-		    From ! {self(),{answer,correct}},
-		    send_all(Players,{self(),{From,wins}}),
-		    W = new_word(Words,Players),
-		    loop(Players,Words,W)
+		    Points = length(Actual),
+		    From ! {Self,{answer,correct,Points}},
+		    Name = get_name(Players,From),
+		    Play = add_points(Players,Name,Points),
+		    async_send_all(Play,{Self,{Name,wins}}),
+		    W2 = new_word(Words,Play),
+		    loop(Play,Words,W2)
 	    end
-		       
+    after 3000 ->
+	    check_alive(Players,self()),
+	    loop(Players,Words,Actual)
     end.
-
-get_stadistics(Players,all) ->
-    lists:map(fun({_,N,P}) -> {N,P} end,Players);
-get_stadistics([{_,Name,Points}|List],Player) ->
-    if Name == Player ->
-	    {Player,Points};
-       true ->
-	    get_stadistics(List,Player)
-    end.
-
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 %%% misc functions
+
+async_send_all(List,Msg) ->
+    spawn(fun() ->
+		  send_all(List,Msg)
+	  end).
 
 send_all([],_)->
     done;
@@ -123,10 +147,82 @@ select_random(Words) ->
 
 new_word(Words,Players) ->
     W = select_random(Words),
-    send_all(Players,{self(),{word,W}}).
+    send_all(Players,{self(),{word,W}}),
+    W.
+
+get_name([],_) ->
+    "Name not found";
+get_name([{Pid,Name,_}|P],From) ->
+    if From =/= Pid ->
+	    get_name(P,From);
+       true ->
+	    Name
+    end.
+check_player_exists([],_) ->
+    false;
+check_player_exists([{_,Pname,0}|Players],Name) ->
+    if Pname =/= Name ->
+	    check_player_exists(Players,Name);
+       true ->
+	    true
+    end.
+
+get_stadistics(Players,all) ->
+    lists:map(fun({_,N,P}) -> {N,P} end,Players);
+get_stadistics([{_,Name,Points}|List],Player) ->
+    if Name == Player ->
+	    {Player,Points};
+       true ->
+	    get_stadistics(List,Player)
+    end.
+
+remove_player([],_) ->
+    [];
+remove_player(Players,Name) ->
+    remove_player_acumulative([],Players,Name).
+
+remove_player_acumulative(P,[],_) ->
+    P;
+remove_player_acumulative(Izq,[{Pid,Pname,Points}|Players],Name) ->
+    if Pname =/= Name ->
+	    remove_player_acumulative([{Pid,Pname,Points}|Izq],Players,Name);
+       true ->
+	    Izq++Players
+    end.
+	    
+check_alive([],_) -> 
+    ok;
+check_alive([{P,N,_}|Players],From) ->
+    spawn(fun() ->
+		  P ! {From,ping,self()},
+		  receive
+		      {N,pong} ->
+			  ok
+		  after 3000 ->
+			  From ! {removePlayer,N}
+		  end
+	  end),
+    check_alive(Players,From).
+
+add_points([],_,_) ->
+    [];
+add_points(Players,Name,Points) ->
+    add_points_acumulative([],Players,Name,Points).
+
+add_points_acumulative(Izq,[],_,_) ->
+    Izq;
+add_points_acumulative(Izq,[{Ppid,Pname,Ppoint}|Players],Name,Points) ->
+    if Pname =/= Name ->
+	    add_points_acumulative([{Ppid,Pname,Ppoint}|Izq],Players,Name,Points);
+       true ->
+	    Izq++[{Ppid,Pname,Ppoint+Points}|Players]
+    end.
+    
+
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 %%% at the end, loop with "too many" words
 loop() ->
-    loop([],["hola","adios","caracola","juego","divertido"],undefined).
+    loop([],["hola","adios","caracola","juego","divertido"],undefined),
+    io:format("muero2~n").
